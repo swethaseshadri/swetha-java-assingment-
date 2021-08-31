70fe26666fe95804d237fa9e55217951d6288f68
swethaseshadri <89858840+swethaseshadri@users.noreply.github.com>
2021-08-31 21:44:00 +0530

    first assignment commit

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int a=100;
+		float b=22.45391f;
+		double c=991;
+		char d='B';
+		String e="Java";
+		
+		
+		System.out.println("int is "+a);
+	      System.out.println(b);
+	      System.out.println(c);
+	      System.out.println(d);
+	      System.out.println(e);
+
+	}
+
+}
---
+++ Sizedataty.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class Sizedataty {
+
+	public static void main(String[] args) {
+		System.out.println("Character " + Character.BYTES+" Bytes");
+        System.out.println("Byte " + Byte.BYTES+" Bytes");
+        System.out.println("Integer " + Integer.BYTES+" Bytes");
+        System.out.println("Long " + Long.BYTES+" Bytes");
+        System.out.println("Short " + Short.BYTES+" Bytes");
+        System.out.println("Double " + Double.BYTES+" Bytes");
+        System.out.println("Float " + Float.BYTES+" Bytes");
+	}
+
+}
